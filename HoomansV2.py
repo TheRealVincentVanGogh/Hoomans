b4de9f60217a56f703b99562edfcae832229da4a
Vincent <vincentye@icloud.com>
2020-01-21 09:35:49 -0800

    Add Death and improvements
    ADD: Death by too old
    MODIFY: create_hooman to adapt to child creation as well as human creation.

--- HoomansV2.py
+++ HoomansV2.py
@@ -4,9 +4,10 @@ import random
 import time
 import copy
 
+max_age = 70
 min_reproduction_age = 18
-initial_men = 5
-initial_female = 5
+initial_men = 3
+initial_female = 3
 population = pd.DataFrame(
     columns=[
     'First Name', 
@@ -17,15 +18,30 @@ population = pd.DataFrame(
     ]
     )
 
+def random_gender():
+    isMale = bool(random.getrandbits(1))
+    if isMale == True:
+        return 'male'
+    elif isMale == False:
+        return 'female'
+
 def get_name(gender):
     ''' Generates a full name split into first + last, array format '''
     return names.get_full_name(gender=gender).split(" ", 1)
 
-def create_hooman(gender):
+def get_first_name(my_gender):
+    ''' Generates exclusively a first name based on gender '''
+    return names.get_first_name(gender=my_gender)
+
+def create_hooman(gender, lastname=''):
     ''' Initiates human attributes, returns as list '''
     attributes = []
-
-    first_name, last_name = get_name(gender)
+ 
+    if lastname == '':
+        first_name, last_name = get_name(gender)
+    else:
+        first_name = get_first_name(gender)
+        last_name = lastname
     age = 0
     virgin = True
 
@@ -50,6 +66,14 @@ def initialize_population():
         hooman_attributes = create_hooman('female')
         #print(hooman_attributes)
         population.loc[len(population), :] = hooman_attributes
+
+def initialize_child(last_name):
+    ''' Initialize a human child inheriting a lastname.'''
+    ''' Adds child to population table '''
+    global population
+
+    child_attributes = create_hooman(random_gender(), lastname=last_name)
+    population.loc[len(population), :] = child_attributes
 
 def agify(df):
     ''' For each human row, increases age by one year '''
@@ -82,10 +106,22 @@ def reproduce(df):
                 mate_first_name = mate.at[0, 'First Name']
                 mate_last_name = mate.at[0, 'Last Name']
                 print(f"{first_name} {last_name} did the deed with {mate_first_name} {mate_last_name}")
+                initialize_child(last_name)
 
+def check_death(df):
+    for index, row in df.iterrows():
+        try:
+            if df.at[index, 'Age'] >= max_age:
+                print(f"{df.at[index, 'First Name']} {df.at[index, 'Last Name']} died at age {df.at[index, 'Age']}!")
+                df = df.drop(index, inplace=True)
+        except:
+            pass
 
 def simulate_year(df):
     ''' Simulate one year of life '''
+    #df = df.reset_index(drop=True)
+    #df.reset_index(drop=True, inplace=True)
+    check_death(df)
     agify(df)
     reproduce(df)
     print(population)
@@ -98,7 +134,7 @@ def main():
 
     while True:
         simulate_year(population)
-        time.sleep(2)
+        time.sleep(0.5)
     
 
 main()
